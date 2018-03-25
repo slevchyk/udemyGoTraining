242ce5cf01347f08c31e15148ec12a0375518482
slevchyk <s.levchyk@gmail.com>
2018-03-25 23:28:03 +0300

    lesson 05 completed

---
+++ lesson05/p01httpGetWithErrorChack/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"io/ioutil"
+	"fmt"
+)
+
+func main()  {
+
+	res, err := http.Get("https://twitter.com/")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	page, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", page)
+
+}
+
