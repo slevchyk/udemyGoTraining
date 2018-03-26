effc0c7ccf204eaa60c015b9cb2f71e1bff62914
slevchyk <s.levchyk@gmail.com>
2018-03-26 18:52:03 +0300

    lesson 12 completed:

---
+++ lesson12/p01initializationStatement/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var didgit int32
+
+	fmt.Print("Enter 'a' code didgit: ")
+	fmt.Scanln(&didgit)
+
+	if char := string(didgit); char == "a" {
+		fmt.Println("Yes, it's a code")
+	}
+}
---
+++ lesson12/p02ifElse/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var age int
+
+	fmt.Print("Enter your age: " )
+	fmt.Scanln(&age)
+
+	if age < 10 {
+		fmt.Println("Your age is less 10 years")
+	} else if age >= 10 && age < 40 {
+		fmt.Println("Your age is 10 or more but less 40")
+	} else if age >= 40 && age < 60 {
+		fmt.Println("Your age is 40 or more but less 60")
+	} else {
+		fmt.Println("Your age is 60 or more")
+	}
+}
